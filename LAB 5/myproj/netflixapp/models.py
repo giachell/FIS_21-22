e024d89e6b94d79f9d934c3a80eb989869c49dc5
Fabio Giachelle <giachelle.fabio@gmail.com>
2021-11-18 18:44:51 +0100

    Initial commit

---
+++ LAB 5/myproj/netflixapp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Movie(models.Model):
+    """A data structure for Netflix movies and series"""
+    title = models.CharField(max_length=1000, blank=False)
+    type = models.CharField(max_length=1000, blank=False)
+    description = models.CharField(max_length=1000, blank=True)
+    director = models.CharField(max_length=1000, blank=True)
+    country = models.CharField(max_length=1000, blank=True)
+    cast = models.CharField(max_length=1000, blank=True)
+    date_added = models.DateField(null=True, blank=True)
+    release_year = models.IntegerField(blank=True)
+    rating = models.CharField(max_length=1000, blank=True)
+    duration = models.CharField(max_length=1000, blank=True)
+    listed_in = models.CharField(max_length=1000, blank=True)
---
+++ README.md
@@ -0,0 +1,2 @@
+# FIS_21-22
+ This repository contains the source code for the lab sessions of the course "Fundamentals of Information Systems". The purpose of this repo is to provide a gentle introduction to web application development using Django. 
